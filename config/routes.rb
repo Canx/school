475c49b662265629ea0d45056af6286668bdec3d
Ruben Cancho <canchete@gmail.com>
2012-05-22 08:06:24 +0200

    Testing bootstrap twitter with main page test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Schooleando::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  root :to => 'welcome#index'
   match 'search' => 'search#show'
   
   resources :schools
@@ -51,7 +52,6 @@ Schooleando::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
