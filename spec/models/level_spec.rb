4da566d62db2559d1c32fb7b721f19cdb79c5876
ruben <ruben@hal9001.(none)>
2012-10-06 19:59:06 +0200

    Added new failing test to level model

--- spec/models/level_spec.rb
+++ spec/models/level_spec.rb
@@ -2,10 +2,13 @@ require 'spec_helper'
 
 describe Level do
   it "should not allow two levels with the same name" do
-    infantil1 = Level.new(:name => "infantil")
-    infantil2 = Level.new(:name => "infantil")
-    infantil1.save
-    infantil2.save
-    infantil2.should have(1).error_on(:name)
+    create(:level, :name => "infantil")
+    expect { create(:level, :name => "infantil") }.to raise_error
+  end
+
+  it "should not allow create a new level with a non-existent parent" do
+    level0 = create(:level, :name => "non-saved level")
+    level0.delete
+    expect { create(:level, :name => "nivel1", :parent => level0) }.to raise_error
   end
 end
