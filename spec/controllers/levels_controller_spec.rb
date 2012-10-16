8d8f96c093f6cb3962f661edd1d39a809b99d1e6
Ruben <ruben@Studio.(none)>
2012-10-16 23:20:23 +0200

    LevelsController#show now returns sublevels

--- spec/controllers/levels_controller_spec.rb
+++ spec/controllers/levels_controller_spec.rb
@@ -12,6 +12,7 @@ describe LevelsController do
     FactoryGirl.create_list(:school, 4, city: @city, levels: [@infantil])
     FactoryGirl.create_list(:school, 5, city: @city, levels: [@bachiller, @bachiller_humanistico])
     FactoryGirl.create_list(:school, 6, city: @city, levels: [@bachiller, @bachiller_cientifico])
+    FactoryGirl.create_list(:school, 7, city: @city, levels: [@bachiller, @bachiller_cientifico, @bachiller_humanistico])
   end
 
   describe "GET 'show'" do
@@ -20,7 +21,7 @@ describe LevelsController do
         get 'show', :id => @infantil.id, :city_id => @city.id
       end
 
-      it "returns all cities with that level" do
+      it "returns all schools with that level and city" do
         assigns(:schools).count.should eq(4) 
       end
     end
@@ -31,7 +32,11 @@ describe LevelsController do
       end
 
       it "returns schools and a list of sublevels of that level" do
-        pending
+        assigns(:level).should eq(@bachiller)
+        assigns(:city).should eq(@city)
+        assigns(:sublevels).should include(@bachiller_cientifico, @bachiller_humanistico)
+        assigns(:sublevels).size.should eq(2)
+        assigns(:schools).size.should eq(18)
       end
     end
   end
@@ -43,10 +48,9 @@ describe LevelsController do
       end
 
       it "returns a list of root levels with schools in that city" do
-        levels = assigns(:levels).map { |l| l.name }
-
-        levels.should include("Infantil","Bachiller")
-        levels.count.should eq(2)
+        assigns(:city).should eq(@city)
+        assigns(:levels).should include(@infantil, @bachiller)
+        assigns(:levels).size.should eq(2)
       end
 
       it "returns a city model" do
